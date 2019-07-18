ee12b870344cf16685ac296c4e4314671acb510c
aronnoapu <aronnoapucse@gmail.com>
2019-07-18 09:54:45 +0300

    integer value change

---
+++ addition.java
@@ -0,0 +1,11 @@
+    public class addition 
+    {
+        public static void main(String[] args)
+         {
+            
+            int first = 30;
+            int second = 40;
+            int sum = first + second;
+            System.out.println("The sum is: " + sum);
+        }
+    }
